2bac1780fdd021c37cec4ca840b7ff1de138cf51
StefWe <35935077+StefWe@users.noreply.github.com>
2023-12-30 23:11:03 +0100

    add json output format

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,10 @@ mkdir -p "$GITHUB_WORKSPACE/threat-model/report"
 
 if [ $# -eq 1 ]; then
   /usr/share/tic-taac/bin/tic-taac.sh --threatModel "$GITHUB_WORKSPACE/$1" --out "$GITHUB_WORKSPACE/threat-model/report"
+  /usr/share/tic-taac/bin/tic-taac.sh --threatModel "$GITHUB_WORKSPACE/$1" --outFormat json --out "$GITHUB_WORKSPACE/threat-model/report"
 fi
 
 if [ $# -eq 2 ]; then
   /usr/share/tic-taac/bin/tic-taac.sh --threatModel "$GITHUB_WORKSPACE/$1" --mitigations "$GITHUB_WORKSPACE/$2" --out "$GITHUB_WORKSPACE/threat-model/report"
+  /usr/share/tic-taac/bin/tic-taac.sh --threatModel "$GITHUB_WORKSPACE/$1" --mitigations "$GITHUB_WORKSPACE/$2" --outFormat json --out "$GITHUB_WORKSPACE/threat-model/report"
 fi
